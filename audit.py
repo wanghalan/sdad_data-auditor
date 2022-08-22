2e04942ee8b90603eefda70955d6fa849489fcdd
Alan <ahw9f@virginia.edu>
2022-08-22 12:32:04 -0400

    updating distribution stipulations: the path should be related to the raw data path, and the data under distributions/ should all be tracked

--- audit.py
+++ audit.py
@@ -3,13 +3,13 @@ import hashlib
 import argparse
 import json
 import logging
+from pathlib import Path
 
 '''
 Given the path to a directory, generate a manifest file
+[Finding files recursively] https://stackoverflow.com/questions/2186525/how-to-use-glob-to-find-files-recursively
 '''
 
-dirs_to_audit = ['code', 'data', 'docs']
-file_end_to_record=['xz']
 
 def get_md5(filepath):
     return hashlib.md5(open(filepath, 'rb').read()).hexdigest()
@@ -22,28 +22,29 @@ def make_website():
     pass
 
 
-def evaluate_folder(data, dirpath):
+def evaluate_folder(data, dirpath, prepend='https://raw.githubusercontent.com/uva-bi-sdad/'):
     '''
     Given a directory, add all file information into data
+    Assume if it is under a distribution folder, to store the manifest for this data
     '''
     logging.debug('Evaluating folder: %s' % dirpath)
-    for child_file in os.listdir(dirpath):
-        logging.debug('\tEvaluating: %s' % child_file)
-        filepath = os.path.join(dirpath, child_file)
-        if os.path.isdir(filepath):
-            evaluate_folder(data, filepath)
-        if os.path.isfile(filepath) and child_file.split('.')[-1] in file_end_to_record:
+
+    for path in Path(dirpath).rglob('distribution/**/*'):
+        logging.debug('\tEvaluating: %s' % path.name)
+
+        if os.path.isfile(path):
             data.append(
                 {
-                    'name': child_file,
-                    'path': os.path.relpath(filepath),
-                    'md5': get_md5(filepath),
-                    'size': os.path.getsize(filepath),
+                    'name': path.name,
+                    # Append the raw github location, add the repository name after, and then add the path under the repository
+                    'path': prepend + os.path.join(os.path.basename(os.path.abspath(args.input_root)), os.path.relpath(path)),
+                    'md5': get_md5(path),
+                    'size': os.path.getsize(path),
                 }
             )
 
 
-def main(root, test=False):
+def main(root, prepend, test=False):
     '''
     Iterate through each file in the repository and check a hash
     '''
@@ -56,9 +57,9 @@ def main(root, test=False):
     for file in os.listdir(root):
         logging.debug(file)
         dirpath = os.path.join(root, file)
-        if os.path.isdir(dirpath) and file in dirs_to_audit:
+        if os.path.isdir(dirpath):
             logging.debug('%s is a related directory' % (dirpath))
-            evaluate_folder(data, dirpath)
+            evaluate_folder(data, dirpath, prepend)
 
     answer['data'] = data
     answer['count'] = len(data)
@@ -69,7 +70,8 @@ def main(root, test=False):
         export_file = os.path.join(root, 'manifest.json')
         with open(export_file, 'w') as f:
             json.dump(answer, f)
-        logging.info('[%s] Manifest file created' % os.path.isfile(export_file))
+        logging.info('[%s] Manifest file created' %
+                     os.path.isfile(export_file))
 
 
 if __name__ == '__main__':
@@ -81,6 +83,8 @@ if __name__ == '__main__':
                         action=argparse.BooleanOptionalAction)
     parser.add_argument('-t', '--test',
                         action=argparse.BooleanOptionalAction)
+    parser.add_argument('-p', '--prepend', type=str,
+                        help='Raw github url to prepend')
 
     args = parser.parse_args()
     log_level = logging.INFO
@@ -89,8 +93,14 @@ if __name__ == '__main__':
 
     logging.basicConfig(format='%(levelname)s: %(message)s', level=log_level)
 
+    prepend = None
+    if args.prepend:
+        prepend = args.prepend
+    else:
+        prepend = 'https://raw.githubusercontent.com/uva-bi-sdad/'
+
     if not os.path.isdir(args.input_root):
         logging.info('%s is not a directory' % (args.input_root))
     else:
         logging.info('Auditing: %s' % os.path.abspath(args.input_root))
-        main(args.input_root, args.test)
+        main(args.input_root, prepend, args.test)
