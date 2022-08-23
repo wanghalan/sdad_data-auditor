40f271f1b9ed9b958be88f33541fe800eff70912
Alan <ahw9f@virginia.edu>
2022-08-23 15:14:40 -0400

    measure info append with >50% cutoff added

--- audit.py
+++ audit.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import logging
 from pathlib import Path
+from fuzzywuzzy import fuzz, process
 
 '''
 Given the path to a directory, generate a manifest file
@@ -13,13 +14,6 @@ Given the path to a directory, generate a manifest file
 
 def get_md5(filepath):
     return hashlib.md5(open(filepath, 'rb').read()).hexdigest()
-
-
-def make_website():
-    '''
-    import plotly and make something?
-    '''
-    pass
 
 
 def evaluate_folder(data, dirpath):
@@ -33,15 +27,38 @@ def evaluate_folder(data, dirpath):
         logging.debug('\tEvaluating: %s' % path.name)
 
         if os.path.isfile(path):
-            data.append(
-                {
-                    'name': path.name,
-                    # Append the raw github location, add the repository name after, and then add the path under the repository
-                    'path': os.path.relpath(path),
-                    'md5': get_md5(path),
-                    'size': os.path.getsize(path),
-                }
-            )
+            parent_dir = path.parent
+            to_append = {
+                'name': path.name,
+                # Append the raw github location, add the repository name after, and then add the path under the repository
+                'path': os.path.relpath(path),
+                'md5': get_md5(path),
+                'size': os.path.getsize(path)
+            }
+
+            measure_data = None
+            # Check if there is a manifest file. If so, then try to append the measure info
+            if 'measure_info.json' in os.listdir(parent_dir):
+                measure_data = search_measure_info(
+                    path, os.path.join(parent_dir, 'measure_info.json'))
+                if measure_data is not None:
+                    to_append['measure_info'] = measure_data
+                else:
+                    to_append['measure_info'] = 'No match found'
+        data.append(to_append)
+
+def search_measure_info(path, measure_info_path, cutoff=50):
+    '''
+    Find the measure info that most match the file, then append that element to the data. Disregard if the final value is less than the cutoff
+    '''
+    with open(measure_info_path, 'r') as f:
+        measure_info = json.load(f)
+
+    keys = measure_info.keys()
+    closest_match = process.extractOne(path.name, keys)
+    logging.debug(closest_match)
+    if closest_match[1] > cutoff:
+        return measure_info[closest_match[0]]
 
 
 def main(root, test=False):
